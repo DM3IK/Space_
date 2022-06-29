45db7085031ea3ae53cbdd6d4b2873fa84455534
Georgevas97 <92178958+Georgevas97@users.noreply.github.com>
2022-06-29 15:10:34 +0200

    modifica script audioplayer

--- space/Assets/Scripts/AudioPlayer.cs
+++ space/Assets/Scripts/AudioPlayer.cs
@@ -6,12 +6,32 @@ public class AudioPlayer : MonoBehaviour
 {
     [Header("Shooting")]
     [SerializeField] AudioClip shootingClip;
-    [SerializeField] [Range(0f, 1f)] float shootingVolume = 1f;
+    [SerializeField][Range(0f, 1f)] float shootingVolume = 1f;
 
     [Header("Damage")]
     [SerializeField] AudioClip damageClip;
-    [SerializeField] [Range(0f, 1f)] float damageVolume = 1f;
+    [SerializeField][Range(0f, 1f)] float damageVolume = 1f;
 
+    static AudioPlayer instance;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     public void PlayShootingClip()
     {
